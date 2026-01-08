fd83275dba69e394f542ef31d8b80edc24a80437
maheenur13 <differentmahee13@gmail.com>
2026-01-08 21:51:45 +0600

    Integrate Tavily API for web search functionality in the chat application. Updated main logic to handle messages and tool calls, and modified getGroqChatCompletion to accept messages as a parameter. Added new dependencies in package.json and package-lock.json.

--- app.js
+++ app.js
@@ -1,23 +1,93 @@
 import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
 
+const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 //
 export async function main() {
-  const chatCompletion = await getGroqChatCompletion();
+  const messages = [
+    {
+      role: "system",
+      content: `You are a smart personal assistant who answers the asked questions.
+      You have access to following tools:
+      1. web_search({query}:{query:string}) // Search the latest information and realtime data on the internet.
+      `,
+    },
+
+    {
+      role: "user",
+      content: "When iphone 17 launched?",
+    },
+  ];
+  const chatCompletion = await getGroqChatCompletion(messages);
+  console.log(1, chatCompletion.choices[0].message);
+
+  messages.push(chatCompletion.choices[0].message);
+
+  const toolCalls = chatCompletion.choices[0]?.message.tool_calls;
+  if (!toolCalls) {
+    console.log(`AI: ${chatCompletion.choices[0]?.message.content}`);
+
+    return;
+  }
+  for (const tool of toolCalls) {
+    const functionName = tool.function.name;
+    const functionParams = tool.function.arguments;
+    console.log(functionParams);
+
+    if (functionName === "web_search") {
+      console.log("calling web search...");
+
+      const toolResult = await web_search(JSON.parse(functionParams));
+
+      messages.push({
+        tool_call_id: tool.id,
+        role: "tool",
+        name: functionName,
+        content: toolResult,
+      });
+    }
+  }
+
+  const chatCompletion2 = await getGroqChatCompletion(messages);
+  console.log(JSON.stringify(chatCompletion2.choices[0].message));
+
   // Print the completion returned by the LLM.
-  console.log(chatCompletion.choices[0]?.message?.content || "");
+  // console.log(chatCompletion.choices[0]?.message?.tool_calls);
 }
 
-export async function getGroqChatCompletion() {
+export async function getGroqChatCompletion(messages) {
   return groq.chat.completions.create({
-    messages: [
+    // temperature: 0,
+    messages,
+    model: "llama-3.3-70b-versatile",
+    tools: [
       {
-        role: "user",
-        content: "Explain the importance of fast language models",
+        type: "function",
+        function: {
+          name: "web_search",
+          description: `Search the latest information and realtime data on the internet.`,
+          parameters: {
+            // JSON Schema object
+            type: "object",
+            properties: {
+              query: {
+                type: "string",
+                description: "The search query to perform search on.",
+              },
+            },
+            required: ["query"],
+          },
+        },
       },
     ],
-    model: "openai/gpt-oss-20b",
+    tool_choice: "auto",
   });
+}
+
+async function web_search({ query }) {
+  const response = await tvly.search(query, {});
+  return response.results.map((itm) => itm.content).join("\n\n");
 }
 
 // Run the main function
